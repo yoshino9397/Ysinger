7ef2af4486fdd7a5fb4e27c4c89598d87fc0997b
yoshino9397 <yayamayoshino@gmail.com>
2021-09-18 22:49:20 -0700

    todaylast

--- js/web.js
+++ js/web.js
@@ -14,3 +14,28 @@ let h1 = "YURIA WAKAI";
 let h2 = "Soprano Singer";
 document.getElementById("name").innerHTML = h1;
 document.getElementById("job").innerHTML = h2;
+
+let works = ["PLAYING", "SLEEPY", "FALL", "MEMORY"];
+ul = document.createElement("ul");
+document.getElementById("disco").appendChild(ul);
+works.forEach(function (music) {
+  let li = document.createElement("li");
+  ul.appendChild(li);
+  li.innerHTML += music;
+});
+
+let h3a = "Discography";
+let h3b = "Biography";
+let h4 = "More Information";
+document.getElementById("h3a").innerHTML = h3a;
+document.getElementById("h3b").innerHTML = h3b;
+document.getElementById("h4").innerHTML = h4;
+
+let contacts = ["FACEBOOK", "INSTAGRAM", "SOUNDS CLOUD", "YOUTUBE"];
+ul = document.createElement("ul");
+document.getElementById("contacts").appendChild(ul);
+contacts.forEach(function (contact) {
+  let li = document.createElement("li");
+  ul.appendChild(li);
+  li.innerHTML += contact;
+});
